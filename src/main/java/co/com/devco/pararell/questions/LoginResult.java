ce08f2d29b797741008e7831baad9d1ebc43ccea
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2022-01-23 19:34:26 -0500

    Cambios de prueba

--- src/main/java/co/com/devco/pararell/questions/LoginResult.java
+++ src/main/java/co/com/devco/pararell/questions/LoginResult.java
@@ -2,15 +2,20 @@ package co.com.devco.pararell.questions;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static co.com.devco.pararell.userinterfaces.ProductPage.BTN_ADD_TO_CART;
 import static co.com.devco.pararell.userinterfaces.ProductPage.BTN_OPEN_SHOP_CART;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isPresent;
 
 public class LoginResult implements Question<Boolean> {
 
     @Override
     public Boolean answeredBy(Actor actor) {
-        return BTN_OPEN_SHOP_CART.resolveFor(actor).isVisible();
+        actor.attemptsTo(
+                WaitUntil.the(BTN_OPEN_SHOP_CART, isPresent()).forNoMoreThan(20).seconds()
+        );
+        return BTN_OPEN_SHOP_CART.resolveFor(actor).isPresent();
     }
 
     public static LoginResult successful(){
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,20 +1,21 @@
 Feature: Login in Swag Labs app
 
-  Background:
+#  Background:
+#    Given That user open Swag Labs app
+
+  @locked
+  Scenario: Login Locked
     Given That user open Swag Labs app
+    When he submit username and password
+      | username      | password     |
+      | locked_out_user | secret_sauce |
+    Then he should be error message
 
   @successful
   Scenario: Login successfull
-#    Given That user open Swag Labs app
+    Given That user open Swag Labs app
     When he submit username and password
       | username      | password     |
       | standard_user | secret_sauce |
     Then he should be logged in
 
-  @locked
-  Scenario: Login Locked
-#    Given That user open Swag Labs app
-    When he submit username and password
-      | username      | password     |
-      | locked_out_user | secret_sauce |
-    Then he should be error message
